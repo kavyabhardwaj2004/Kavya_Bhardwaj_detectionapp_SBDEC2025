20929eeeee9cd10966ab24d44991c5971f82998c
kavyabhardwaj <kbspace2004@gmail.com>
2025-12-05 19:00:56 +0530

    webcam streaming functionality using OpenCV

--- practice_sessions/1.py
+++ practice_sessions/1.py
@@ -1,1 +1,17 @@
+# for webcam #
+import cv2
+# For USB webcam (index 0 = first camera)
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("Camera Stream", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
